6eee69743d70747918789c8a3d6552b2dbe60480
ojtaylor123 <ojtaylor.ot@googlemail.com>
2022-11-15 11:18:14 +0000

    task 5 completed

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -73,3 +73,50 @@ describe("GET /api/reviews", () => {
       });
   });
 });
+
+describe("get reviews by id", () => {
+  test("for an api call for a review ID that is given as a non integer, should return 400 and an invalid ID message", () => {
+    return request(app)
+      .get("/api/reviews/hello")
+      .expect(400)
+      .then(({ body }) => {
+        expect(body.msg).toBe("Invalid query review ID must be int");
+      });
+  });
+
+  test("for an api call for a review_id that is an int but has no response as there are no reviews associated with it ", () => {
+    return request(app)
+      .get("/api/reviews/78")
+      .expect(404)
+      .then(({ body }) => {
+        expect(body.msg).toBe("review ID not found");
+      });
+  });
+
+  test("should return associated reviews when given a valid ID", () => {
+    return request(app)
+      .get("/api/reviews/2")
+      .expect(200)
+      .then(({ body }) => {
+        const { review } = body;
+        expect(Array.isArray(review)).toBe(true);
+        expect(review.length).toBe(1);
+        expect(review[0] && typeof review[0] === "object").toBe(true);
+
+        expect(review[0]).toMatchObject({
+          review_id: expect.any(Number),
+          title: expect.any(String),
+          review_body: expect.any(String),
+          designer: expect.any(String),
+          review_img_url: expect.any(String),
+          votes: expect.any(Number),
+          category: expect.any(String),
+          owner: expect.any(String),
+          created_at: expect.any(String),
+
+
+        });
+
+      });
+  });
+});
