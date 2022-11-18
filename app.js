304b7bb7c9e701e02fdd4006bfd4d41579f8b623
ojtaylor123 <ojtaylor.ot@googlemail.com>
2022-11-18 12:01:27 +0000

    done

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const express = require("express");
 const app = express();
 app.use(express.json());
 
-app.get('/api', getEndpoints)
+//app.get('/api', getEndpoints)
 
 app.get("/api/categories", getCategories);
 
